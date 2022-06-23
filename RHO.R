016a83cc2a7148709bda630c4fcc91ad043e299f
WW92030 <65929722+WW92030-STORAGE@users.noreply.github.com>
2022-06-23 08:05:59 -0500

    Create RHO.R

---
+++ RHO.R
@@ -0,0 +1,32 @@
+hex <- function(n) { # Integer to hex string
+    n <- n %% 256
+    p1 <- n %/% 16
+    p2 <- n %% 16
+    c1 <- paste(p1)
+    c2 <- paste(p2)
+    
+    if (p1 >= 10) c1 <- intToUtf8(p1 - 10 + 65)
+    if (p2 >= 10) c2 <- intToUtf8(p2 - 10 + 65)
+    
+    return(paste(c1, c2, sep = ""))
+}
+
+primagen <- function() { # Generate an array of 3 random byte integers
+    return(floor(runif(3, 0, 256)))
+}
+
+protogen <- function() { # Generate a random hex color
+    rgb <- floor(runif(3, 0, 256))
+    return(paste("\U0023", hex(rgb[1]), hex(rgb[2]), hex(rgb[3]), sep = ""))
+}
+
+toString <- function(rgb) { # Convert RGB array into hex string
+  return(paste("\U0023", hex(rgb[1]), hex(rgb[2]), hex(rgb[3]), sep = ""))
+}
+
+inv <- function(rgb) { # Invert an RGB array.
+  return(c(255 - rgb[1], 255 - rgb[2], 255 - rgb[3]))
+}
+
+for (x in 1 : 16) print(protogen())
+
